4c34a1a01c55b751bce70375c3e6905448a5bf4c
Job Kilonzo <jobkilonzo95@gmail.com>
2023-02-13 18:30:28 +0300

    change port number

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require("express")
 const programRouter = require("./routes/programs")
 const app = express()
 const mongoose = require("mongoose")
+const PORT  = process.env.PORT || 3001
 
 mongoose.connect(process.env.DATABASE_URL)
 mongoose.set('strictQuery', true)
@@ -11,4 +12,4 @@ db.on('error', (error) => console.error(error))
 db.once('open', () => console.log('Connected to Database'))
 app.use(express.json())
 app.use("/programs", programRouter)
-app.listen(3000, () => console.log("Server started"))
+app.listen(PORT, () => console.log("Server started"))
